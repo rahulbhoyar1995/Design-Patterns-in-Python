1acb98c47e3c3d41111826eb1c04638bfa804841
Rahul Bhoyar <85449261+rahulbhoyar1995@users.noreply.github.com>
2024-04-24 20:54:32 +0200

    Update 5_dependenacy_inversion_principle.py

--- 2_solid_design_principles/5_dependenacy_inversion_principle.py
+++ 2_solid_design_principles/5_dependenacy_inversion_principle.py
@@ -1,5 +1,10 @@
 # Dependecy Inversion Principle
 
+# High-level modules should not depend on low-level modules: In traditional programming, higher-level modules often depend directly on lower-level modules. For example, a business logic module might directly depend on a database access module. This creates tight coupling, making it difficult to change or replace either module without affecting the other.
+# Both high-level and low-level modules should depend on abstractions: Instead of depending on specific implementations, modules should depend on interfaces or abstract classes. This allows for interchangeable components. Going back to the previous example, the business logic module would depend on an interface for data access rather than a specific database access module.
+# Abstractions should not depend on details: The interfaces or abstract classes used for dependencies should not be tied to specific implementations or details. They should define what functionality is needed without dictating how it's implemented. This allows for different implementations to be used interchangeably.
+# By following the Dependency Inversion Principle, you make your code more flexible, maintainable, and easier to test. It promotes modular design and makes it easier to swap out components without affecting the rest of the system
+
 from abc import abstractmethod
 from enum import Enum
 
